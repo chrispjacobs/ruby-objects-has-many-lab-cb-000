0487bf645f09c76496f284d20076d5bc7bf52f38
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-27 15:06:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
     attr_accessor :name
+    @@all_artists_songs_array = []
     def initialize(name)
       @name = name
       @songs_array = []
@@ -11,6 +12,7 @@ class Artist
     #   puts "song_variable inputted reads: #{song_variable}"
     #   puts "songs_array initially reads: #{@songs_array}"
       @songs_array << song_variable
+      @@all_artists_songs_array << song_variable
     #   puts "songs_array after shoveling reads: #{@songs_array}"
     #   puts "song_variable.artist initially reads: #{song_variable.artist}"
       if song_variable.artist == nil
@@ -21,6 +23,7 @@ class Artist
     def add_song_by_name(name_string)
       new_song_variable = Song.new(name_string)
       @songs_array << new_song_variable
+      @@all_artists_songs_array << new_song_variable
       # puts "We just initialized #{new_song_variable}"
       # puts "new_song_variable.name returns #{new_song_variable.name}"
       # puts "new_song_variable.artist initially returns #{new_song_variable.artist}"
@@ -31,5 +34,8 @@ class Artist
     end
     def song_count
       @songs_array.count
+    end
+    def self.song_count
+      @@all_artists_songs_array
     end
   end
