c712a858e60c17b1eb4d89ffdc2102c26d15e91f
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-27 14:55:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,12 +20,19 @@ class Artist
     end
     def add_song_by_name(name_string)
       new_song_variable = Song.new
+      puts "We just initialized #{new_song_variable}"
       new_song_variable.name = name_string
+      puts "new_song_variable.name returns #{new_song_variable.name}"
+      puts "new_song_variable.artist initially returns #{new_song_variable.artist}"
       if new_song_variable.artist == nil
         new_song_variable.artist = self
+        puts "new_song_variable.artist now returns #{new_song_variable.artist}"
       end
     end
     def song_count
       @songs_array.count
     end
   end
+
+the_beatles = Artist.new("The Beatles")
+the_beatles.add_song_by_name("Help!")
