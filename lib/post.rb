ffd74815199786a8115037e960ba486fd1f5f84c
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-27 18:13:39 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,10 @@
 class Post
   @@all = []
-  @author = nil
   attr_accessor :title
   def initialize(title)
     @title = title
-    @all << self
+    @author = nil
+    @@all << self
   end
   def author=(author_variable)
     @author = author_variable
