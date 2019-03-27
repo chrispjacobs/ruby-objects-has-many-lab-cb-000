fd3d0f00e591c6eb958ae60063f12d5d1731cf25
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-27 18:25:57 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,8 +14,7 @@ class Author
       end
     end
     def add_post_by_title(post_title)
-      post_variable = Post.new
-      post_variable.title = post_title
+      post_variable = Post.new(post_title)
       @posts << post_variable
     end
     def posts
