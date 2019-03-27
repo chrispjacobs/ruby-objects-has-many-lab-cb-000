9f0e3e1a022feb1624390bbe392e88d9c8488312
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-27 18:21:05 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,24 +1,27 @@
 class Author
-  attr_accessor :name
-  def initialize(name)
-    @name = name
-    @posts = []
-  end
-  def posts
-    @posts
-  end
-  def add_post(post_variable)
-    @posts << post_variable
-    if post_variable.author_name = nil
-      post_variable.author = self
+    attr_accessor :name
+    def initialize(name)
+      @name = name
+      @posts = []
+    end
+    def posts
+      @posts
+    end
+    def add_post(post_variable)
+      @posts << post_variable
+      if post_variable.author_name == nil
+        post_variable.author = self
+      end
+    end
+    def add_post_by_title(post_title)
+      post_variable = Post.new
+      post_variable.title = post_title
+      @posts << post_variable
+    end
+    def posts
+        @posts
+    end
+    def post_count
+      @posts.count
     end
   end
-  def add_post_by_title(post_title)
-    post_variable = Post.new
-    post_variable.title = post_title
-    @posts << post_variable
-  end
-  def post_count
-    @posts
-  end
-end
