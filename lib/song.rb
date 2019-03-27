21946a88ae0c571504cff1b053935f8d9bea0389
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-27 15:25:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,26 @@
 class Song
-  @@all = []
-  attr_accessor :name, :artist
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-  def self.all
-    @@all
-  end
-  def artist=(artist)
-    @artist = artist
-    artist.add_song(self)
-  end
-  def artist_name
-    if self.artist != nil
-      if self.artist.name != nil
-        self.artist.name
+    @@all = []
+    attr_accessor :name, :artist
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+    def artist=(artist)
+        @artist = artist
+        if artist.songs.include? self == false
+          artist.add_song(self)
+        end
+    end
+    def self.all
+      @@all
+    end
+    def artist_name
+      if self.artist != nil
+        if self.artist.name != nil
+          self.artist.name
+        end
+      else
+        nil
       end
-    else
-      nil
     end
   end
-end
